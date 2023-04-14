abc176748d1aab192cc42797e78c6d90d0eaff07
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-14 16:16:32 -0400

    Adds first routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import './styles/App.css'
+import { Routes, Route } from 'react-router'
+import Header from './components/header'
 
 function App() {
+  const [token, setToken] = useState(null)
 
-
-  return null
+  return (
+    <Routes>
+      <Route element={<Header />}>
+        <Route path='/' element={token ? <Home /> : <Landing />} />
+        
+      </Route>
+    </Routes>
+  )
   
 }
 
