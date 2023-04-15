2f34806b29badf8d9d9d0febde5d926a2ed0e1c9
Jeeglobs <dp.hudgins@hotmail.com>
2023-04-15 18:28:46 -0400

    add models to admin.py

--- backend/potluck/admin.py
+++ backend/potluck/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Event, Invitation, Item, Post
 
 admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Invitation)
+admin.site.register(Item)
+admin.site.register(Post)
