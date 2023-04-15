9b9809e45471ad72bfcc8c37431f205f611de99b
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-15 00:43:25 -0400

    Adds routes for event form, deets, rsvp, invitations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import './styles/App.css'
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/header'
-import useLocalStorage from 'react-use-localstorage';
 import { AuthContext } from './context/authcontext';
+import useLocalStorage from 'react-use-localstorage';
+
 import ProtectedRoute from './components/protectedroute';
+import Header from './components/header'
+import EventForm from './components/eventform'
+import Login from './pages/login';
+import SignUp from './pages/sign-up';
+import EventDetails from './pages/eventdetails';
+import Invitations from './pages/invitations';
+import RSVPList from './pages/rsvplist';
 
 function App() {
   const [token, setToken] = useLocalStorage('token', null)
@@ -14,8 +21,14 @@ function App() {
         <Route element={<Header setToken={setToken} />}>
           <Route path='/' element={token ? <Home /> : <Landing/>} />
           <Route path='/login' element={<Login setToken={setToken} />} />
-          <Route path='/sign-up' element={<SignUp setToken={setToken}/>} />
-          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-up' element={<SignUp setToken={setToken} />} />
+          <ProtectedRoute>
+            <Route path='/invitations' element={<Invitations />} />
+            <Route path='/event/new' element={<EventForm />} />
+            <Route path='/event/:pk' element={<EventDetails />} />
+            <Route path='/event/:pk/invitations' element={<RSVPList />} />
+          </ProtectedRoute>
+
           
         </Route>
       </Routes>
