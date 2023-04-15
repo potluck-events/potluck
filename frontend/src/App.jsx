bd8717fbfd8643924cbac9e5e106dd30e72b1ad5
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-15 00:30:27 -0400

    Creates routes for index, login, signup, & context provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react'
 import './styles/App.css'
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/header'
+import useLocalStorage from 'react-use-localstorage';
+import { AuthContext } from './context/authcontext';
+import ProtectedRoute from './components/protectedroute';
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useLocalStorage('token', null)
 
   return (
-    <Routes>
-      <Route element={<Header />}>
-        <Route path='/' element={token ? <Home /> : <Landing />} />
-        
-      </Route>
-    </Routes>
+    <AuthContext.Provider value={token}>
+      <Routes>
+        <Route element={<Header setToken={setToken} />}>
+          <Route path='/' element={token ? <Home /> : <Landing/>} />
+          <Route path='/login' element={<Login setToken={setToken} />} />
+          <Route path='/sign-up' element={<SignUp setToken={setToken}/>} />
+          <Route path='/sign-up' element={<SignUp />} />
+          
+        </Route>
+      </Routes>
+    </AuthContext.Provider>
   )
   
 }
+
 
 export default App
