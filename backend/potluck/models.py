2798418c65c9f75af7ffd62099a3a14009e6ae29
Jeeglobs <dp.hudgins@hotmail.com>
2023-04-15 17:34:25 -0400

    add state CHOICES to Event Model

--- backend/potluck/models.py
+++ backend/potluck/models.py
@@ -7,13 +7,74 @@ class User(AbstractUser):
 
 
 class Event(models.Model):
+    CHOICES = (
+        ('', ''),
+        ('alabama', 'AL'),
+        ('alaska', 'AK'),
+        ('arizona', 'AZ'),
+        ('arkansas', 'AR'),
+        ('american-samoa', 'AS'),
+        ('california', 'CA'),
+        ('colorado', 'CO'),
+        ('connecticut', 'CT'),
+        ('delaware', 'DE'),
+        ('district-of-columbia', 'DC'),
+        ('florida', 'FL'),
+        ('georgia', 'GA'),
+        ('guam', 'GU'),
+        ('hawaii', 'HI'),
+        ('idaho', 'ID'),
+        ('illinois', 'IL'),
+        ('indiana', 'IN'),
+        ('iowa', 'IA'),
+        ('kansas', 'KS'),
+        ('kentucky', 'KY'),
+        ('louisiana', 'LA'),
+        ('maine', 'ME'),
+        ('maryland', 'MD'),
+        ('massachusetts', 'MA'),
+        ('michigan', 'MI'),
+        ('minnesota', 'MN'),
+        ('mississippi', 'MS'),
+        ('missouri', 'MO'),
+        ('montana', 'MT'),
+        ('nebraska', 'NE'),
+        ('nevada', 'NV'),
+        ('new-hampshire', 'NH'),
+        ('new-jersey', 'NJ'),
+        ('new-mexico', 'NM'),
+        ('new-york', 'NY'),
+        ('north-carolina', 'NC'),
+        ('north-dakota', 'ND'),
+        ('northern-mariana-islands', 'MP'),
+        ('ohio', 'OH'),
+        ('oklahoma', 'OK'),
+        ('oregon', 'OR'),
+        ('pennsylvania', 'PA'),
+        ('puerto-rico', 'PR'),
+        ('rhode-island', 'RI'),
+        ('south-carolina', 'SC'),
+        ('south-dakota', 'SD'),
+        ('tennessee', 'TN'),
+        ('texas', 'TX'),
+        ('trust-territories', 'TT'),
+        ('utah', 'UT'),
+        ('vermont', 'VT'),
+        ('virginia', 'VA'),
+        ('virgin-islands', 'VI'),
+        ('washington', 'WA'),
+        ('west-virginia', 'WV'),
+        ('wisconsin', 'WI'),
+        ('wyoming', 'WY'),
+    )
+
     title = models.CharField(max_length=50)
     theme = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
     location_name = models.CharField(max_length=50)
     street_address = models.CharField(max_length=50)
     city = models.Charfield(max_length=50)
-    state = models.Charfield(max_length=2)
+    state = models.Charfield(choices=CHOICES, max_length=2)
     zipcode = models.Charfield(max_length=20)
     date = models.DateField()
     time = models.TimeField()
