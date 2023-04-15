5f14011a2696c0db410124f4e5f09e6414b59c9c
Jeeglobs <dp.hudgins@hotmail.com>
2023-04-15 18:44:00 -0400

    finish up Models

--- backend/potluck/models.py
+++ backend/potluck/models.py
@@ -9,79 +9,18 @@ class User(AbstractUser):
     thumbnail = models.ImageField(blank=True, null=True)
     phone_number = models.CharField(max_length=10)
     # email already included with AbstractUser?
-    city = models.Charfield(max_length=50)
+    city = models.CharField(max_length=50)
 
 
 class Event(models.Model):
-    CHOICES = (
-        ('', ''),
-        ('alabama', 'AL'),
-        ('alaska', 'AK'),
-        ('arizona', 'AZ'),
-        ('arkansas', 'AR'),
-        ('american-samoa', 'AS'),
-        ('california', 'CA'),
-        ('colorado', 'CO'),
-        ('connecticut', 'CT'),
-        ('delaware', 'DE'),
-        ('district-of-columbia', 'DC'),
-        ('florida', 'FL'),
-        ('georgia', 'GA'),
-        ('guam', 'GU'),
-        ('hawaii', 'HI'),
-        ('idaho', 'ID'),
-        ('illinois', 'IL'),
-        ('indiana', 'IN'),
-        ('iowa', 'IA'),
-        ('kansas', 'KS'),
-        ('kentucky', 'KY'),
-        ('louisiana', 'LA'),
-        ('maine', 'ME'),
-        ('maryland', 'MD'),
-        ('massachusetts', 'MA'),
-        ('michigan', 'MI'),
-        ('minnesota', 'MN'),
-        ('mississippi', 'MS'),
-        ('missouri', 'MO'),
-        ('montana', 'MT'),
-        ('nebraska', 'NE'),
-        ('nevada', 'NV'),
-        ('new-hampshire', 'NH'),
-        ('new-jersey', 'NJ'),
-        ('new-mexico', 'NM'),
-        ('new-york', 'NY'),
-        ('north-carolina', 'NC'),
-        ('north-dakota', 'ND'),
-        ('northern-mariana-islands', 'MP'),
-        ('ohio', 'OH'),
-        ('oklahoma', 'OK'),
-        ('oregon', 'OR'),
-        ('pennsylvania', 'PA'),
-        ('puerto-rico', 'PR'),
-        ('rhode-island', 'RI'),
-        ('south-carolina', 'SC'),
-        ('south-dakota', 'SD'),
-        ('tennessee', 'TN'),
-        ('texas', 'TX'),
-        ('trust-territories', 'TT'),
-        ('utah', 'UT'),
-        ('vermont', 'VT'),
-        ('virginia', 'VA'),
-        ('virgin-islands', 'VI'),
-        ('washington', 'WA'),
-        ('west-virginia', 'WV'),
-        ('wisconsin', 'WI'),
-        ('wyoming', 'WY'),
-    )
-
     title = models.CharField(max_length=50)
     theme = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField(max_length=200)
     location_name = models.CharField(max_length=50)
     street_address = models.CharField(max_length=50)
-    city = models.Charfield(max_length=50)
-    state = models.Charfield(choices=CHOICES, max_length=2)
-    zipcode = models.Charfield(max_length=20)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    zipcode = models.CharField(max_length=20)
     date = models.DateField()
     time = models.TimeField()
     host = models.ForeignKey(
@@ -99,7 +38,8 @@ class Invitation(models.Model):
     class Meta:
         constraints = [
             UniqueConstraint(
-                fields=['event', 'guest']
+                fields=['event', 'guest'],
+                name='invitation_constraints'
             )
         ]
 
