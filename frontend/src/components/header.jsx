37fb9fc9788cfa7a7cbc96170bc513eba491fc3e
Michael Greason <mgreason18@gmail.com>
2023-04-16 13:01:51 -0400

    style changes

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,41 +1,16 @@
 import { Outlet } from 'react-router-dom'
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
 import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-import { Avatar, Typography } from "@material-tailwind/react";
-
-const products = [
-  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
-  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
-  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
-  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
-  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
-]
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-
+import { Avatar, Typography, Button } from "@material-tailwind/react";
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
         <>
-    <header className="bg-cyan-100">
+    <header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5 flex">
@@ -49,7 +24,7 @@ export default function Header() {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             onClick={() => setMobileMenuOpen(true)}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
   <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
 </svg>
           </button>
@@ -62,15 +37,15 @@ export default function Header() {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="bg-cyan-100 flex items-center justify-between">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="bg-white flex items-center justify-between">
             <a href="/" className="-m-1.5 p-1.5 flex">
               <img
-                className="h-8 w-auto mr-1.5 "
+                className="h-10 w-auto mr-1.5 "
                 src="/temp-img/champagne-glasses.png"
                 alt=""
               />
-            <span className='mt-1'>PotLuck</span>
+            <span className='mt-2 text-xl'>PotLuck</span>
             </a>
             <button
               type="button"
@@ -78,13 +53,13 @@ export default function Header() {
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <XMarkIcon className="h-8 w-8" aria-hidden="true" />
             </button>
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-            <div className="flex gap-4">
-            <Avatar className="mt-4" src="/temp-img/winnie.png" t="avatar" size="md" variant="circular"  />
+            <div className="columns-1 flex my-8 justify-center">
+            <Avatar className="h-80 aspect-square rounded-full w-auto mt-4" src="/temp-img/winnie.png" t="avatar" size="md" variant="circular"  />
             </div>
               <Typography variant='h1' className="py-6 text-center" color="black">
                 NAME
@@ -103,7 +78,9 @@ export default function Header() {
               <Typography variant='paragraph' className="text-center mb-5" color="black">
                Location of user
               </Typography>
-           
+              <div className="columns-1 flex justify-center mt-20">
+                <Button variant='filled' color='blue'>Log Out</Button>
+            </div>
             </div>
           </div>
         </Dialog.Panel>
