2a0544bf78031776088d5ec09b1ab6e160513a20
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-16 14:04:57 -0400

    Saves latest pipenv installs

--- backend/potluck/views.py
+++ backend/potluck/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from dj_rest_auth.registration.views import SocialLoginView
 
+# if you want to use Authorization Code Grant, use this
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = CALLBACK_URL_YOU_SET_ON_GOOGLE
+    client_class = OAuth2Client
+
+
+class GoogleLogin(SocialLoginView):  # if you want to use Implicit Grant, use this
+    adapter_class = GoogleOAuth2Adapter
 # Create your views here.
