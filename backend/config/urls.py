a4db33a98830f6ad740596039db11cdd5c8836f8
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-16 13:48:15 -0400

    Adds DJRA urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/google/', GoogleLogin.as_view(), name='google_login')
 ]
