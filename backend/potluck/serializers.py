4a72c540b2615a23449dc6f6d7f8495df1f595d1
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-16 21:08:57 -0400

    Adds custom sign up serializer

---
+++ backend/potluck/serializers.py
@@ -0,0 +1,11 @@
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+
+class CustomRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+
+    def custom_signup(self, request, user):
+        user.first_name = self.validated_data['first_name']
+        user.last_name = self.validated_data['last_name']
+        user.save(update_fields=['first_name', 'last_name'])
