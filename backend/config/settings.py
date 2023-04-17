68aad66f5f75ce6e8ff90ce9c1c1dfd80acb282a
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-17 00:17:05 -0400

    Sets social and login settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+
     'rest_framework',
     'rest_framework.authtoken',
 
@@ -62,16 +63,32 @@ INSTALLED_APPS = [
 
     'corsheaders',
     'phonenumber_field',
+
     'potluck',
 ]
 
 # DJ REST AUTH SITE ID
 SITE_ID = 1
-
-#Users will login with emails, emails not verified
+LOGIN_REDIRECT_URL = '/'
+# Users will login with emails, emails not verified
+SOCIALACCOUNT_QUERY_EMAIL = True
 ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
