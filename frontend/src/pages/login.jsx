8f458d7acf3bb4b538e57e0d49ffa6bcec743019
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-17 15:10:21 -0400

    Adds login html and function

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,20 +1,23 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import axios from "axios";
 import { useState } from "react";
+import { Typography, Button, Input } from "@material-tailwind/react";
 
 
-
-export default function Login(setToken) {
+export default function Login({setToken}) {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const [email, setEmail] = useState(null)
-  const [password, setPassword] = useState(null)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
 
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault()
+
     const options = {
       method: 'POST',
-      url: 'http://127.0.0.1:8080/dj-rest-auth/login/',
+      url: 'http://127.0.0.1:8000/dj-rest-auth/login/',
       headers: {
         'Content-Type': 'application/json'
       },
@@ -25,16 +28,41 @@ export default function Login(setToken) {
     };
 
     axios.request(options).then((response) => {
+      console.log(setToken);
       console.log(response.data);
+      setToken('Token ' + response.data.key);
+      const origin = location.state?.from?.pathname || '/'
+      navigate(origin)
     }).catch((error) => {
       console.error(error);
+      setError(error.response.data)
     }); 
 
 
     //this code sends you to the page the user was redirected from visa-cis the ProtectedRoute component if it is stored in the location context
-    const origin = location.state?.from?.pathname || '/'
-    navigate(origin)
   }
 
-  return null
+  return (
+    <>
+      <div className="mt-8 flex flex-col items-center justify-center">
+        <Typography variant = 'h4' color="blue-gray">Login to your account</Typography>
+        <form onSubmit={(e) => handleLogin(e)}>
+          <div className="mt-8 mb-4 w-80">
+            <div className="flex flex-col gap-6">
+              <div>
+                <Input required value={email} onChange={(e) => setEmail(e.target.value)} label="Email" size="lg" />
+                {error.email && <Typography variant='small' color="red">{error.email[0]}</Typography>}
+              </div>
+              <div>
+                <Input required value={password} onChange={(e) => setPassword(e.target.value)} label="Password" size="lg" type="password" />
+                {error.non_field_errors && <Typography variant='small' color="red">Your password and/or username were incorrect. Please login again.</Typography>}
+              </div>
+            <Button type="submit" className="" fullWidth>Login</Button>
+            </div>
+          </div>
+          <Typography variant = "small">Don't have an account? <Link to="/sign-up" className=" font-bold text-blue-800">Create one</Link></Typography>
+        </form>
+      </div>
+  
+    </>)
 }
