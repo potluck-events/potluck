6467ca4056cff13b6b008f324281a9d45d4c0daf
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-17 17:52:11 -0400

    starts Form

--- frontend/src/components/eventform.jsx
+++ frontend/src/components/eventform.jsx
@@ -1,5 +1,13 @@
 
 
 export default function EventForm() {
+  const [title, setTitle] = useState('')
+  const [theme, setTheme] = useState('')
+  const [description, setDescription] = useState('')
+  const [locationName, setLocationName] = usState('')
+  const [street, setStreet] = useState('')
+  const [city, setCity] = useState('')
+
+
   return null
 }
