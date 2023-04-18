fd7881e7edd4f23f06ac1dcbd6e85f695e029a25
Jeeglobs <dp.hudgins@hotmail.com>
2023-04-18 10:23:26 -0400

    change events/create to events

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     path('events/hosting', views.EventsHosting.as_view()),
     path('events/attending', views.EventsAttending.as_view()),
     path('items', views.Items.as_view()),
-    path('events/create', views.CreateEvent.as_view()),
+    path('events', views.CreateEvent.as_view()),
     path('events/<int:pk>', views.EventDetails.as_view()),
 ]
