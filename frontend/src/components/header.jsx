e1aecabd1157be7f900e77aeac747915b6e2f5d5
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-19 22:05:41 -0400

    Hides prof icon

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -7,6 +7,8 @@ import {
 import { Avatar, Button, Typography,  } from "@material-tailwind/react";
 import { AuthContext } from '../context/authcontext';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header({setToken}) {
   const token = useContext(AuthContext)
@@ -38,62 +40,38 @@ export default function Header({setToken}) {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': token
-  }
-}).then((response) => {
-  setUserData(response.data)
-})
-.catch(error => {
-  console.error(error);
-});
-}, [])
-
-console.log(userData)
-
-  function UserName() {
-    return (
-      <div className="py-6 text-center" color="black">
-      <Typography variant='h2'>{userData.first_name} {userData.last_name}</Typography>
-      </div>
-    )
-  }
-
-  function EmailAddress() {
-    return (
-      <div className="text-center" color="black">
-      <Typography variant='paragraph' className='font-semibold'>{userData.email}</Typography>
-      </div>
-    )
-  }
-
-  function UserLocation() {
-    return (
-      <div className="text-center mb-5" color="black">
-      <Typography variant='paragraph' className='font-semibold'>{userData.city} </Typography>
-      </div>
-    )
-  }
+    }
+    }).then((response) => {
+      setUserData(response.data)
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, [])
+  
+ 
   
   return (
     <>
       <header className="bg-white">
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5 flex">
+            <Link to="/" className="-m-1.5 p-1.5 flex">
               <img className="h-10 w-auto mr-1.5" src="/temp-img/champagne-glasses.png" alt="" />
               <span className='mt-2 text-xl'>PotLuck</span>
-            </a>
+            </Link>
           </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
-                <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
-              </svg>
-            </button>
-          </div>
+          {token &&
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <FontAwesomeIcon icon={faUser} className='w-6 h-6' />
+              </button>
+            </div>
+          }
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
             <button
                 type="button"
@@ -132,18 +110,18 @@ console.log(userData)
                 <div className="columns-1 flex my-8 justify-center">
                   <Avatar className="h-80 aspect-square rounded-full w-auto mt-4" src="/temp-img/winnie.png" t="avatar" size="md" variant="circular"  />
                 </div >
-                    <UserName/>
+                  <UserName userData={ userData} />
                   <div>
                     <Typography variant='paragraph' className="text-center" color="gray">
                       Email: 
                     </Typography>
-                      <EmailAddress />
+                      <EmailAddress userData={ userData} />
                   </div>
                   <div>
                     <Typography variant='paragraph' className="text-center pt-6" color="gray">
                       Location: 
                     </Typography>
-                      <UserLocation/>
+                      <UserLocation userData={ userData} />
                   </div>
                   <div className="columns-1 flex justify-center mt-7">
                     <Button variant='filled' className='w-44' color='blue' onClick={handleLogout}>Log Out</Button>
@@ -157,3 +135,27 @@ console.log(userData)
     </>
     )
 }
+
+ function UserName({userData}) {
+    return (
+      <div className="py-6 text-center" color="black">
+      <Typography variant='h2'>{userData.first_name} {userData.last_name}</Typography>
+      </div>
+    )
+  }
+
+  function EmailAddress({userData}) {
+    return (
+      <div className="text-center" color="black">
+      <Typography variant='paragraph' className='font-semibold'>{userData.email}</Typography>
+      </div>
+    )
+  }
+
+  function UserLocation({userData}) {
+    return (
+      <div className="text-center mb-5" color="black">
+      <Typography variant='paragraph' className='font-semibold'>{userData.city} </Typography>
+      </div>
+    )
+  }
