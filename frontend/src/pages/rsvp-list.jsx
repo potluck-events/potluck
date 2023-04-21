859e07bed36b89e217c90df8dfec0da55b3a91dc
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-21 00:03:42 -0400

    Simplifies components

--- frontend/src/pages/rsvp-list.jsx
+++ frontend/src/pages/rsvp-list.jsx
@@ -5,70 +5,6 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import Invitation from "../components/event-details/event-invitation";
 
-// useEffect(() => {
-//   null
-// })
-
-  function EventTitle(){
-    return (
-      <div className="text-center my-2" color="black">
-      <Typography variant='h3'>Event Title</Typography>
-      </div>
-    )
-  }
-
-  function Invitations(){
-    return (
-      <div className='flex justify-between mx-5'>
-        <Typography variant='h4'>Invitations</Typography>
-        <Typography variant='h4'># Invites</Typography>
-      </div>
-    )
-  }
-
-  function InviteButton({setInviteModalOpen}){
-    return (
-      <div className='mx-4 my-4'>
-        <Button onClick={() => setInviteModalOpen(true)} fullWidth>Invite Guests</Button>
-      </div>
-    )
-  }
-  
-  function Attending(){
-    return (
-      <div className='mx-5 my-5'>
-        <Typography variant='h4'>Attending</Typography>
-          <div className='mx-5 my-2'>
-            <Typography variant='paragraph' className='font-semibold'>Full Name</Typography>
-            <Typography variant='paragraph'>Email</Typography>
-          </div>
-      </div>
-    )
-  }
-
-  function TBD(){
-    return (
-      <div className='mx-5 my-5'>
-        <Typography variant='h4'>TBD</Typography>
-          <div className='mx-5 my-2'>
-            <Typography variant='paragraph' className='font-semibold'>Full Name</Typography>
-            <Typography variant='paragraph'>Email</Typography>
-          </div>
-      </div>
-    )
-  }
-  
-  function Declined(){
-    return (
-      <div className='mx-5 my-5'>
-        <Typography variant='h4'>Declined</Typography>
-          <div className='mx-5 my-2'>
-            <Typography variant='paragraph' className='font-semibold'>Full Name</Typography>
-            <Typography variant='paragraph'>Email</Typography>
-          </div>
-      </div>
-    )
-  }
 
 export default function RSVPList() {
   const [inviteModalOpen, setInviteModalOpen] = useState(false)
@@ -86,3 +22,64 @@ export default function RSVPList() {
   )
 }
 
+
+function EventTitle(){
+  return (
+    <div className="text-center my-2" color="black">
+    <Typography variant='h3'>Event Title</Typography>
+    </div>
+  )
+}
+
+function Invitations(){
+  return (
+    <div className='flex justify-between mx-5'>
+      <Typography variant='h4'>Invitations</Typography>
+      <Typography variant='h4'># Invites</Typography>
+    </div>
+  )
+}
+
+function InviteButton({setInviteModalOpen}){
+  return (
+    <div className='mx-4 my-4'>
+      <Button onClick={() => setInviteModalOpen(true)} fullWidth>Invite Guests</Button>
+    </div>
+  )
+}
+
+function Attending(){
+  return (
+    <div className='mx-5 my-5'>
+      <Typography variant='h4'>Attending</Typography>
+        <div className='mx-5 my-2'>
+          <Typography variant='paragraph' className='font-semibold'>Full Name</Typography>
+          <Typography variant='paragraph'>Email</Typography>
+        </div>
+    </div>
+  )
+}
+
+function TBD(){
+  return (
+    <div className='mx-5 my-5'>
+      <Typography variant='h4'>TBD</Typography>
+        <div className='mx-5 my-2'>
+          <Typography variant='paragraph' className='font-semibold'>Full Name</Typography>
+          <Typography variant='paragraph'>Email</Typography>
+        </div>
+    </div>
+  )
+}
+
+function Declined(){
+  return (
+    <div className='mx-5 my-5'>
+      <Typography variant='h4'>Declined</Typography>
+        <div className='mx-5 my-2'>
+          <Typography variant='paragraph' className='font-semibold'>Full Name</Typography>
+          <Typography variant='paragraph'>Email</Typography>
+        </div>
+    </div>
+  )
+}
