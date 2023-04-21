950858a1c41ae1c7cab34fffd7b799501d35064d
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-21 14:48:09 -0400

    adds get for rsvps

--- frontend/src/pages/rsvp-list.jsx
+++ frontend/src/pages/rsvp-list.jsx
@@ -1,13 +1,32 @@
 import { Typography, IconButton, Button } from "@material-tailwind/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useContext, useState } from "react";
 import Invitation from "../components/event-details/event-invitation";
+import axios from "axios";
+import { AuthContext } from "../context/authcontext";
 
 
 export default function RSVPList() {
   const [inviteModalOpen, setInviteModalOpen] = useState(false)
+  const { pk } = useParams()
+  const token = useContext(AuthContext)
+  
+  const options = {
+  method: 'GET',
+  url: `https://potluck.herokuapp.com/events/${pk}/invitations`,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: token
+  }
+};
+
+axios.request(options).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
 
   return (
   <>
@@ -48,10 +67,10 @@ function InviteButton({setInviteModalOpen}){
   )
 }
 
-function Attending(){
+function Attending({header, users}){
   return (
     <div className='mx-5 my-5'>
-      <Typography variant='h4'>Attending</Typography>
+      <Typography variant='h4'>{header}</Typography>
         <div className='mx-5 my-2'>
           <Typography variant='paragraph' className='font-semibold'>Full Name</Typography>
           <Typography variant='paragraph'>Email</Typography>
