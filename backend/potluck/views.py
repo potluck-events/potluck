95e1118c458c40f1b4ee12f594603b51cff3e259
Jeeglobs <dp.hudgins@hotmail.com>
2023-04-24 14:45:54 -0400

    add MultiPartParser to UserProfile views

--- backend/potluck/views.py
+++ backend/potluck/views.py
@@ -1,4 +1,4 @@
-
+from rest_framework.parsers import MultiPartParser
 
 # AUTHENTICATION IMPORTS
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
@@ -68,6 +68,7 @@ def CodeView(request):
 
 class UserProfile(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
+    parser_classes = [MultiPartParser]
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
