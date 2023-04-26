2f807dff8f40b2190d2876134f61a1c83421419e
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-26 14:09:03 -0400

    fixes location display

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -53,7 +53,7 @@ export default function Profile() {
                 </Typography>
                 <EmailAddress user={user} />
             </div>
-            <div  className="py-2"v>
+            <div  className="py-2">
                 <Typography variant='paragraph' className="text-center" color="gray">
                     Dietary Restrictions: 
                 </Typography>
@@ -61,8 +61,8 @@ export default function Profile() {
                             <Link to={'/profile/edit'}><Typography variant='paragraph' className='text-center text-light-blue-800 italic'>Add Dietary Restrictions</Typography></Link>}
             </div>
                     
-            { user.location && <div>
-                <Typography variant='paragraph' className="text-center pt-6" color="gray">
+            { user.city && <div className="py-2">
+                <Typography variant='paragraph' className="text-center" color="gray">
                     Location: 
                 </Typography>
                 <UserLocation user={user} />
