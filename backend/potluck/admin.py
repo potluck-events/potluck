af73dccb6dfc4bceb595d56af5a0cdd02d6d5442
Jeeglobs <dp.hudgins@hotmail.com>
2023-04-27 16:31:13 -0400

    add Notification Model to admin.py

--- backend/potluck/admin.py
+++ backend/potluck/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, DietaryRestriction, Event, Invitation, Item, Post
+from .models import User, DietaryRestriction, Event, Invitation, Item, Post, Notification
 
 admin.site.register(User)
 admin.site.register(DietaryRestriction)
@@ -7,3 +7,4 @@ admin.site.register(Event)
 admin.site.register(Invitation)
 admin.site.register(Item)
 admin.site.register(Post)
+admin.site.register(Notification)
