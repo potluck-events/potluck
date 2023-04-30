8c7256b3525031e5cbe7652c63dc3eaac6dee149
Michael Greason <mgreason18@gmail.com>
2023-04-30 18:55:01 -0400

    fix pagination bug on notifications page

--- frontend/src/pages/notifications.jsx
+++ frontend/src/pages/notifications.jsx
@@ -15,7 +15,7 @@ import { useParams, useNavigate } from 'react-router-dom'
 
 export default function Notifications(){
     const token = useContext(AuthContext)
-    const [notifications, setNotifications] = useState()
+    const [notifications, setNotifications] = useState([])
     const [event, setEvent] = useState()
     const { pk } = useParams()
     const navigate = useNavigate()
@@ -30,7 +30,7 @@ export default function Notifications(){
             }
         }).then((response) => {
             console.log(response.data)
-            setNotifications(response.data)
+            setNotifications(response.data.results)
         })
     }, [])
         
