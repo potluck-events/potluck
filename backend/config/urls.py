358319b46dec39c45e747a8bb91d1b5d47f74291
Jeeglobs <dp.hudgins@hotmail.com>
2023-05-01 16:14:01 -0400

    reorganize imports in urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# django imports
 from django.contrib import admin
 from django.urls import path, include
+
+# third-party imports
+from dj_rest_auth.registration.views import VerifyEmailView
+from dj_rest_auth.views import PasswordResetConfirmView, PasswordResetView
+
+# local app imports
 from potluck import views
-from dj_rest_auth.views import (
-    PasswordResetConfirmView, PasswordResetView
-)
-from dj_rest_auth.registration.views import (
-    RegisterView, VerifyEmailView
-)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
