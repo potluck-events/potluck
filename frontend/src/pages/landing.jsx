d8c7deaf4db6042b110e4e179584ebfcc40b6b6d
Michael Greason <mgreason18@gmail.com>
2023-05-02 14:16:49 -0400

    landing page looks AMAZING

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -40,9 +40,6 @@ export default function Landing() {
           color="blue"
         >
           Effortless Party Planning.
-          <div className="absolute -right-10 top-40 w-2/5 -z-50">
-            <img src="/temp-img/image.png" className="w-2/3 rotate-45"></img>
-          </div>
         </Typography>
       </div>
       <div className="">
@@ -50,21 +47,20 @@ export default function Landing() {
           Welcome to Bash{" "}
           <FontAwesomeIcon className="self-center ml-1" icon={faArrowRight} />
         </Typography>
-        <Typography
-          className="flex mx-6 mt-6"
-          color="black"
-          style={{ marginRight: "35%" }}
-        >
-          Introducing the ultimate party planning app! With our app,
-        </Typography>
-        <Typography className="flex mx-6 mb-6" color="black">
-          {" "}
-          you can easily plan and organize your next event with ease. From
-          creating a guest list and sending out invitations to managing RSVPs,
-          needs, and wants - our app has got you covered. Plus, our intuitive
-          design and user-friendly interface make planning your next bash a
-          breeze. With our app, you'll be the party planning pro in no time!
-        </Typography>
+        <div className="relative overflow-x-clip">
+          <img
+            className="absolute -top-48 right-0 transform rotate-45 w-1/4 z-10 md:hidden"
+            src="/temp-img/image.png"
+          />
+          <Typography className="mx-6 pb-6" color="black">
+            Introducing the ultimate party planning app! With our app, you can
+            easily plan and organize your next event with ease. From creating a
+            guest list and sending out invitations to managing RSVPs, needs, and
+            wants - our app has got you covered. Plus, our intuitive design and
+            user-friendly interface make planning your next bash a breeze. With
+            our app, you'll be the party planning pro in no time!
+          </Typography>
+        </div>
       </div>
       <div className="flex justify-center">
         <img
